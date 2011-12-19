ec7523ae8a86b37e5f07c497e0836244dbccb202
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-12-19 20:00:12 +0900

    chef recipe for 10gen MMS (MongoDB Monitoring System) Agent

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['mms_agent']['api_key'] = ''
+default['mms_agnet']['secret_key'] = ''
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Treasure Data, Inc."
+maintainer_email "k@treasure-data.com"
+license          "All rights reserved"
+description      "Installs/Configures mms-agent"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+depends 'python'
+depends 'runit'
