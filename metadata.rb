04d918cc18befc717744bbd6dac77b5f15f90637
Kevin Reedy <kevinreedy@gmail.com>
2012-12-17 17:01:06 -0600

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "k@treasure-data.com"
 license          "All rights reserved"
 description      "Installs/Configures mms-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
 
 depends 'python'
 depends 'runit'
