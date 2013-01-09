d089f8414052726a6437fda23b52d3050be9ef6b
Chris Lundquist <chris.lundquist@bluebox.net>
2013-01-09 12:57:22 -0800

    [default] add multiple init styles. I don't want runit

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
-default['mms_agent']['api_key'] = ''
-default['mms_agent']['secret_key'] = ''
+default.mms_agent.api_key = ''
+default.mms_agent.secret_key = ''
+default.mms_agent.init_style = 'runit'
+
