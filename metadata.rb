92b331dba365c8ed756654ec04d8336132a4f711
Gregoire Seux <g.seux@criteo.com>
2013-05-14 09:56:15 +0200

    Fix init to correct start and add status

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "k@treasure-data.com"
 license          "All rights reserved"
 description      "Installs/Configures mms-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.5.1"
+version          "0.5.2"
 
 depends 'python'
 depends 'runit'
